45e6f3013fb75d8dcd8d3c6f2619d55c7da28939
Cesanu97 <cesanu1997@gmail.com>
2024-12-23 21:07:39 +0000

    compito buon natale

---
+++ hello/compito.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Variabili per il nome e il cognome
+    char nome[50], cognome[50];
+
+    // Chiedi all'utente di inserire il proprio nome
+    printf("Inserisci il tuo nome: ");
+    scanf("%s", nome);  // Leggi il nome
+
+    // Chiedi all'utente di inserire il proprio cognome
+    printf("Inserisci il tuo cognome: ");
+    scanf("%s", cognome);  // Leggi il cognome
+
+    // Stampa un messaggio di benvenuto 
+    printf("\nBenvenuto, %s %s!\n", nome, cognome);
+
+    // Messaggio di ringraziamento
+    printf("Grazie per aver usato il nostro programma.\n");
+
+    // Messaggio di auguri 
+    printf("Ti auguriamo Buone Feste\n");
+
+    // Albero di Natale
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf("  ***\n");
+
+    return 0;
+}
+
