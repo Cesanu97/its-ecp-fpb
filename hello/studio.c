f8e5551a1641e1b0414d9f7263a27022ad7dc6e5
Cesanu97 <cesanu1997@gmail.com>
2024-12-29 15:31:12 +0000

    Esercizio Natale

---
+++ hello/studio.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+
+int main ()
+{
+    // define variable
+   char nome[50];
+   char cognome[50];
+
+    // ask user to enter his name
+    printf("inserisci il tuo nome: ");
+    scanf("%s", nome); 
+
+    // ask user to enter his surname
+    printf("Inserisci il tuo cognome:  ");
+    scanf("%s", cognome);
+
+    // welcome message
+    printf("\nBenvenuto, %s %s!", nome, cognome);
+
+    // thank the user for using our program
+    printf("\nGrazie per aver utilizzato il nostro programma %s %s!", nome, cognome);
+
+    // happy holidays message
+    printf("\nTi auguriamo Buone Feste!!\n");
+
+    // Christmas tree
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf("  ***\n");
+    
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+    return 0;
+}
