c2f0c9071144417b894726ad097c019739430313
Cesanu97 <cesanu1997@gmail.com>
2025-01-21 16:34:30 +0000

    nuovo esercizio lez4

---
+++ 04/ese_casa_spesa.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+// definizione variabili giorni
+char giorni[64]= "";
+int spesa;
+
+// Richiesta giorno sttimana e totale spesa
+printf("Inserisci giorno della settimana in cui fatto spesa: ");
+scanf("%s", giorni);
+printf("inserisci totale spesa")
+
+
+
+
+
+
+
+
+
+
+
+
+    // Programma terimnato correttamente
+    return 0;
+}
