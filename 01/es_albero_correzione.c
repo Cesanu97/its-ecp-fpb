00fabdeb2e616e04193f14be9e6b71ce54836318
Cesanu97 <cesanu1997@gmail.com>
2025-01-21 16:12:17 +0000

    Correzione compito albero

---
+++ 01/es_albero_correzione.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main(){
+
+// Inizializazione variabili
+char nome [64] = "";
+char cognome [64] = "";
+// messaggio benvenuto e nomi 
+printf("Benvenuto, inserisci nome e cognome: ");
+scanf("%s %s", nome, cognome);
+printf("\nNome e cognome inseriti: %s %s", nome, cognome);
+// Christmas tree
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf("  ***\n");
+
+
+
+
+
+
+
+
+
+
+
+    // Programma terminato correttamente
+    return 0;
+}
