5266d6639ea9d559df08424b2315defcdf91a4ae
Cesanu97 <cesanu1997@gmail.com>
2025-01-29 18:56:15 +0000

    EseFor

--- 05/ese_for.c
+++ 05/ese_for.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 
-int main()
-{
-//inizializazione variabile numero 
-int n_intero;
-
-// Chiedere utente inserire numero int
-printf("Insrisci numero intero: ");
-scanf("%d", &n_intero);
-
-for(; n_intero );
+int main() {
+    int N;
     
+  do {
+        printf("Inserisci un numero intero positivo: ");
+        scanf("%d", &N);
+        if (N <= 0) {
+            printf("Errore! Il numero deve essere positivo.\n");
+        }
+    } while (N <= 0);
 
+    // Stampa il triangolo di numeri
+    for (int i = 1; i <= N; i++) { 
+        for (int j = 1; j <= i; j++) { 
+            printf("%d ", j);
+        }
+        printf("\n"); 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Programma terminato correttamente
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
